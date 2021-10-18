5a532b4c29d0245130aadc97b0ca85ca6f46fe98
mateusparr <mateus.dji@icloud.com>
2021-10-18 11:18:19 -0300

    criando componentes

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { FuncAddComponent } from './func-add/func-add.component';
+import { FuncEditComponent } from './func-edit/func-edit.component';
+import { FuncGetComponent } from './func-get/func-get.component';
+
+const routes: Routes = [
+  {
+    path: 'funcionario',
+    component: FuncGetComponent
+  },
+  {
+    path: 'funcionario/create',
+    component: FuncAddComponent
+  },
+  {
+    path: 'funcionario/edit/:id',
+    component: FuncEditComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
